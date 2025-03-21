a739524b5bb6d8fd66cb2dac9bd029d7eb28df80
Lê Duy Hải <duyha0207@gmail.com>
2025-03-21 23:52:26 +0700

    add project

---
+++ src/main/java/com/duyhai/bookweb_backend/BookwebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.duyhai.bookweb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwebBackendApplication.class, args);
+	}
+
+}
